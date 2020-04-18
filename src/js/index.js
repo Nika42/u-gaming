bcbbc779afed8cce8f972f94a17460f129c5db68
Nika42 <nika.shamanauri@gmail.com>
2020-04-18 14:06:48 +0400

    დაემატა fullstory

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,37 @@ import $ from "jquery";
 import "bootstrap";
 import "popper.js";
 import "owl.carousel";
-
 import ScrollReveal from "scrollreveal";
 
 $(function () {
 	// $('#slider').height(window.innerHeight);
+	if (window.innerWidth <= 990) {
+		$(".filter-row").click(function () {
+			$(".filter-row.is-open").not(this).removeClass("is-open");
+			$(this).toggleClass("is-open");
+		});
+	}
 
-	$("#navbar .fa-search").click(() => {
-		$("#navbar .navbar-brand, #navbar .navbar-nav").addClass("d-none");
+	$(".dropdown-keep-open").on("hide.bs.dropdown", (e) => {
+		if (!e.clickEvent || e.clickEvent.target.tagName === "A") {
+			return true;
+		}
+
+		let target = $(e.clickEvent.target);
+
+		return !(
+			target.hasClass("dropdown-keep-open") || target.parents(".dropdown-keep-open").length
+		);
+	});
+
+	$("#search-init").click(() => {
+		$(".navbar .navbar-nav").addClass("d-none");
 		$("#search-input").fadeIn();
 	});
 
 	$("#search-close").click(() => {
 		$("#search-input").fadeOut("fast", () => {
-			$("#navbar .navbar-brand, #navbar .navbar-nav").removeClass("d-none");
+			$(".navbar .navbar-nav").removeClass("d-none");
 		});
 	});
 
@@ -25,12 +42,12 @@ $(function () {
 	sr.reveal("nav");
 	sr.reveal(".slider-title");
 	sr.reveal(".popular-games .switcher-container");
-	sr.reveal(".section-1 .tab-pane.active .card");
-	sr.reveal(".news");
-	sr.reveal(".ad");
-	sr.reveal(".owl-carousel");
-	sr.reveal(".section-3 .col-3");
-	sr.reveal(".section-4 .container");
+	// sr.reveal(".section-1 .tab-pane.active .card");
+	// sr.reveal(".news");
+	// sr.reveal(".ad");
+	// sr.reveal(".owl-carousel");
+	// sr.reveal(".section-3 .col-3");
+	// sr.reveal(".section-4 .container");
 
 	function ellipsizeText(el) {
 		if ($(el).text().length > 38) {
@@ -46,15 +63,22 @@ $(function () {
 		ellipsizeText(v);
 	});
 
-	var $owl = $(".owl-carousel");
+	$("#related-carousel").owlCarousel({
+		loop: false,
+		margin: 16,
+		items: 4,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fal fa-angle-left"></i>', '<i class="fal fa-angle-right"></i>'],
+	});
 
-	$owl.owlCarousel({
+	$("#main-carousel").owlCarousel({
 		items: 1,
 		loop: false,
 		margin: 0,
 		dots: true,
 		lazyLoad: true,
-		dotsClass: "owl-dots container d-flex justify-content-end",
+		dotsClass: "owl-dots container d-flex justify-content-lg-end",
 	});
 });
 
@@ -67,8 +91,8 @@ function makeViewable() {
 }
 
 $(window).on("load", function () {
-	// setViewable();
-	setTimeout(function () {
-		makeViewable();
-	}, 2500);
+	makeViewable();
+	// setTimeout(function () {
+	// 	makeViewable();
+	// }, 2500);
 });
